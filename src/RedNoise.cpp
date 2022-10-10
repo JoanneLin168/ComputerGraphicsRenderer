aae24a7cdc11617f1312f4bfa0f1599fc67044b3
JoanneLin168 <joannelin168@outlook.com>
2022-10-10 13:26:47 +0100

    Week 3 Task 3 complete

--- src/RedNoise.cpp
+++ src/RedNoise.cpp
@@ -7,6 +7,7 @@
 #include <glm/glm.hpp> // Week 2 - Task 4
 #include <CanvasPoint.h> // Week 3 - Task 2
 #include <Colour.h> // Week 3 - Task 2
+#include <CanvasTriangle.h> // Week 3 - Task 3
 
 #define WIDTH 320
 #define HEIGHT 240
@@ -19,34 +20,6 @@ void draw(DrawingWindow& window) {
 			float red = rand() % 256;
 			float green = 0.0;
 			float blue = 0.0;
-			uint32_t colour = (255 << 24) + (int(red) << 16) + (int(green) << 8) + int(blue);
-			window.setPixelColour(x, y, colour);
-		}
-	}
-}
-
-// Week 2 - Task 3
-void draw(DrawingWindow &window, std::vector<float> pixels) {
-	window.clearPixels();
-	for (size_t y = 0; y < window.height; y++) {
-		for (size_t x = 0; x < window.width; x++) {
-			float red = pixels[x];
-			float green = pixels[x];
-			float blue = pixels[x];
-			uint32_t colour = (255 << 24) + (int(red) << 16) + (int(green) << 8) + int(blue);
-			window.setPixelColour(x, y, colour);
-		}
-	}
-}
-
-// Week 2 - Task 5
-void draw(DrawingWindow& window, std::vector<std::vector<glm::vec3>> pixels) {
-	window.clearPixels();
-	for (size_t y = 0; y < window.height; y++) {
-		for (size_t x = 0; x < window.width; x++) {
-			float red = pixels[y][x][0];
-			float green = pixels[y][x][1];
-			float blue = pixels[y][x][2];
 			uint32_t colour = (255 << 24) + (int(red) << 16) + (int(green) << 8) + int(blue);
 			window.setPixelColour(x, y, colour);
 		}
@@ -74,12 +47,35 @@ void drawLine(DrawingWindow& window, CanvasPoint from, CanvasPoint to, Colour co
 	}
 }
 
+// Week 3 - Task 3
+void drawTriangle(DrawingWindow& window, CanvasPoint a, CanvasPoint b, CanvasPoint c, Colour colour) {
+	drawLine(window, a, b, colour); // Draw line a to b
+	drawLine(window, a, c, colour); // Draw line a to c
+	drawLine(window, b, c, colour); // Draw line b to c
+}
+
+CanvasPoint createRandomPoint() {
+	int x = rand() % WIDTH;
+	int y = rand() % HEIGHT;
+	return CanvasPoint(x, y);
+}
+
+Colour createRandomColour() {
+	int r = rand() % 256;
+	int g = rand() % 256;
+	int b = rand() % 256;
+	return Colour(r, g, b);
+}
+
 void handleEvent(SDL_Event event, DrawingWindow &window) {
 	if (event.type == SDL_KEYDOWN) {
 		if (event.key.keysym.sym == SDLK_LEFT) std::cout << "LEFT" << std::endl;
 		else if (event.key.keysym.sym == SDLK_RIGHT) std::cout << "RIGHT" << std::endl;
 		else if (event.key.keysym.sym == SDLK_UP) std::cout << "UP" << std::endl;
 		else if (event.key.keysym.sym == SDLK_DOWN) std::cout << "DOWN" << std::endl;
+
+		else if (event.key.keysym.sym == SDLK_u)
+			drawTriangle(window, createRandomPoint(), createRandomPoint(), createRandomPoint(), createRandomColour()); // Week 3 - Task 3
 	} else if (event.type == SDL_MOUSEBUTTONDOWN) {
 		window.savePPM("output.ppm");
 		window.saveBMP("output.bmp");
@@ -93,14 +89,22 @@ int main(int argc, char *argv[]) {
 	while (true) {
 		// We MUST poll for events - otherwise the window will freeze !
 		if (window.pollForInputEvents(event)) handleEvent(event, window);
-		CanvasPoint from = CanvasPoint(0, 0);
+
+		// Week 3 - Task 2
+		/*CanvasPoint from = CanvasPoint(0, 0);
 		CanvasPoint to = CanvasPoint(WIDTH, HEIGHT);
 		Colour colour = Colour(255, 0, 0);
-		drawLine(window, from, to, colour);
-		CanvasPoint from2 = CanvasPoint(WIDTH/2, 0);
-		CanvasPoint to2 = CanvasPoint(WIDTH/2, HEIGHT);
-		Colour colour2 = Colour(0, 255, 0);
-		drawLine(window, from2, to2, colour2);
+		drawLine(window, from, to, colour);*/
+
+		// Week 3 - Task 3 pt1 (pt2 is in handleEvent())
+		int w = WIDTH - 1;
+		int h = HEIGHT - 1;
+		CanvasPoint a = CanvasPoint(0, 0);
+		CanvasPoint b = CanvasPoint(w, h/2);
+		CanvasPoint c = CanvasPoint(w/2, h);
+		Colour colour = Colour(255, 0, 0);
+		drawTriangle(window, a, b, c, colour);
+
 		// Need to render the frame at the end, or nothing actually gets shown on the screen !
 		window.renderFrame();
 	}
